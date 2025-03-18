0c03ae2e263f3281a0909029c3bcc3085289caa9
Daniel Kromek <kromekdani@gmail.com>
2025-03-18 21:42:42 +0100

    Generated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, CartComponent, HeaderComponent,
+            LoginComponent, RegisterComponent, PageNotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
