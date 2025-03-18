89120e12c1db546432e112a4f4faac1e059de666
Daniel Kromek <kromekdani@gmail.com>
2025-03-18 23:36:13 +0100

    Fonts and header style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,17 @@ import { CartComponent } from './pages/cart/cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HomeComponent, CartComponent, HeaderComponent,
-            LoginComponent, RegisterComponent, PageNotFoundComponent],
+            LoginComponent, RegisterComponent, PageNotFoundComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dragonstone';
+
+  page = 'home';
 }
