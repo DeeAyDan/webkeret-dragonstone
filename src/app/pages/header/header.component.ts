c79b59429f6ca26004dfb7f5a24950db56bc6ef3
Daniel Kromek <kromekdani@gmail.com>
2025-04-05 16:12:50 +0200

    Buttons now change on page switch

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -7,8 +7,9 @@ import { AfterViewInit, Component, EventEmitter, OnInit, Output } from '@angular
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
-  @Output() selectedPage: EventEmitter<String> = new EventEmitter();
+  @Output() selectedPage: EventEmitter<string> = new EventEmitter();
   isSidebarOpen = false;
+  navButtons: NodeListOf<Element> | null = null;
 
   constructor() {}
 
@@ -17,11 +18,20 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    // Logic that needs to run after the view has been initialized
+    this.navButtons = document.querySelectorAll('.nav-button');
   }
 
-  pageSwitch(page: String) {
-    console.log('Page switched to:', page);
+  pageSwitch(page: string) {
+
+    if (this.navButtons) {
+      this.navButtons.forEach((button) => {
+        button.classList.remove('active');
+        console.log('Button removed:', button);
+      });
+    }
+
+    const selectedButton = document.getElementById(page);
+    selectedButton?.classList.add('active');
     this.selectedPage.emit(page);
   }
 }
