37bf494a776d2ffa09b18a957abbad5d23928eed
Daniel Kromek <kromekdani@gmail.com>
2025-04-08 22:16:08 +0200

    Product detail page funtional

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
 import { CommonModule } from '@angular/common';
 import { FormatPricePipe } from '../../shared/pipes/format-price.pipe';
+import { MatCardModule } from '@angular/material/card';
+import { Review } from '../../models/review';
+import { FormsModule } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [CommonModule, FormatPricePipe],
+  imports: [CommonModule, FormatPricePipe, MatCardModule, FormsModule, MatFormField, MatInputModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
+  newReview: Review = {
+    rating: 5,
+    comment: '',
+    userID: '',
+    productID: '',
+    date: new Date()
+  };
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) {}
+  constructor(private router: Router, private route: ActivatedRoute, private productService: ProductService) {}
 
   ngOnInit() {
     const productId = this.route.snapshot.paramMap.get('id');
@@ -26,5 +38,17 @@ export class ProductDetailsComponent implements OnInit {
 
   addToCart(product: Product) {
     // Implement add to cart functionality here
+  }
+
+  submitReview() {
+    if (this.product && !this.product.reviews) this.product.reviews = [];
+    if (this.product?.reviews) {
+      this.product.reviews.push({ ...this.newReview });
+    }
+    this.newReview = { rating: 5, comment: '', userID: '', productID: '', date: new Date() };
+  }
+
+  goBack() {
+    this.router.navigate(['/shop']);
   }
 }
