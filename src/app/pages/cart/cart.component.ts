3c2fe4d0a2f1a412f39f731efbd9cd7324e70853
Dan <kromekdani@gmail.com>
2025-04-10 11:22:18 +0200

    Pretier formating

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { Cart } from '../../models/cart';
@@ -8,47 +7,56 @@ import { FormatPricePipe } from '../../shared/pipes/format-price.pipe';
 import { ConfirmRemoveDialogComponent } from '../confirm-remove-dialog/confirm-remove-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-cart',
-  imports: [CommonModule, FormatPricePipe, MatButtonModule, MatIconModule, MatTableModule],
+  imports: [
+    CommonModule,
+    FormatPricePipe,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+  ],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
 export class CartComponent implements OnInit {
   cart: Cart | undefined;
-  displayedColumns: string[] = ['image', 'name', 'quantity', 'price', 'subtotal'];
-
+  displayedColumns: string[] = [
+    'image',
+    'name',
+    'quantity',
+    'price',
+    'subtotal',
+  ];
 
   displayItems: {
-    product: Product,
-    quantity: number
+    product: Product;
+    quantity: number;
   }[] = [];
 
-  constructor(private cartService: CartService, private dialog: MatDialog) {
-    
-  }
+  constructor(private cartService: CartService, private dialog: MatDialog) {}
 
   ngOnInit() {
     this.loadCart();
   }
-  
+
   increaseQuantity(product: Product) {
     this.cartService.addToCart(product);
     this.loadCart();
     console.log(this.cartService.getCart('guest'));
   }
-  
+
   decreaseQuantity(product: Product) {
     this.cartService.decreaseQuantity(product);
     this.loadCart();
   }
-  
+
   removeFromCart(product: Product) {
     const dialogRef = this.dialog.open(ConfirmRemoveDialogComponent);
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.cartService.removeFromCart(product);
         this.loadCart();
@@ -58,17 +66,18 @@ export class CartComponent implements OnInit {
 
   loadCart() {
     this.cart = this.cartService.getCart('guest');
-    this.displayItems = this.cart?.items.map(item => {
-      const product = this.cartService.getProductById(item.productID);
-      return {
-        product: product!,
-        quantity: item.quantity
-      };
-    }) || [];
+    this.displayItems =
+      this.cart?.items.map((item) => {
+        const product = this.cartService.getProductById(item.productID);
+        return {
+          product: product!,
+          quantity: item.quantity,
+        };
+      }) || [];
 
     if (this.cart) {
-    this.cart.total = this.cartService.calculateTotal(this.cart);
-  }
+      this.cart.total = this.cartService.calculateTotal(this.cart);
+    }
   }
 
   checkout() {
@@ -76,6 +85,6 @@ export class CartComponent implements OnInit {
   }
 
   totalPrice() {
-  return    this.cartService.calculateTotal(this.cart!);
+    return this.cartService.calculateTotal(this.cart!);
   }
 }
