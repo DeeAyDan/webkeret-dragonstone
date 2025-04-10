122e32ed94fa7a4f9ee8fa56efff1b01d2bbd2d4
Dan <kromekdani@gmail.com>
2025-04-10 14:29:02 +0200

    Prettier

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,8 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  Output,
-} from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
