bde36d8ef756e9702ab586399ab46f5181aad382
Daniel Kromek <kromekdani@gmail.com>
2025-05-16 22:07:21 +0200

    firebase setup + auth service

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,24 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'dragonstone-11f9a',
+        appId: '1:229598369493:web:e224c5a44ca933dc91b45c',
+        storageBucket: 'dragonstone-11f9a.firebasestorage.app',
+        apiKey: 'AIzaSyCvDKROtbsTt-0e5I6Jpx7XWz8-1RiJoF0',
+        authDomain: 'dragonstone-11f9a.firebaseapp.com',
+        messagingSenderId: '229598369493',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 });
